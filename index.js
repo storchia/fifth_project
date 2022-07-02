1cdcac1e484392f38772e59199d4df4239322083
Sergio Andres <sergiotorchia@hotmail.com>
2022-07-01 22:21:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let height = 180;
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let heighInMts = parseFloat(1.8);
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let weight = parseFloat(82.5);
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let heightUp = Math.ceil(heighInMts);
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let weightDown = Math.floor(weight);
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+// No se entiende este ultimo punto
